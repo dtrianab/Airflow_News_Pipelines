07353eabce9fd0a595776f11d769351520cc76c0
Diego Triana <da.trianab@gmail.com>
2021-12-04 14:42:30 +0100

    sql code to create newsapi table, this file is used by postgres_init.py file (initialization DAG)

---
+++ dags/sql/newsapi.sql
@@ -0,0 +1,10 @@
+-- create pet table
+CREATE TABLE IF NOT EXISTS newsapi (
+    title VARCHAR,
+    author VARCHAR,
+    source VARCHAR,
+    description VARCHAR,
+    content VARCHAR,
+    pub_date DATE,
+    url VARCHAR,
+    photo_url VARCHAR);
