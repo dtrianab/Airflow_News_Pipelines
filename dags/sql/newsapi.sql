5d5e83628ce60d137190595a53e94b898cf90992
Diego Triana <da.trianab@gmail.com>
2022-01-16 00:37:58 +0100

    Split column source into source_id and source_name

--- dags/sql/newsapi.sql
+++ dags/sql/newsapi.sql
@@ -2,7 +2,8 @@
 CREATE TABLE IF NOT EXISTS newsapi (
     title VARCHAR,
     author VARCHAR,
-    source VARCHAR,
+    source_id VARCHAR,
+    source_name VARCHAR,
     description VARCHAR,
     content VARCHAR,
     pub_date DATE,
